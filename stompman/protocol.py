a5b8bd13eb6cc7e8efee7519ae6bfcbfc0ccc9de
Lev Vereshchagin <mail@vrslev.com>
2024-06-09 13:03:11 +0300

    Update

--- stompman/protocol.py
+++ stompman/protocol.py
@@ -51,49 +51,11 @@ def dump_frame(frame: BaseFrame[Any]) -> bytes:
     return b"".join(lines)
 
 
-def unescape_header(header_buffer: list[bytes]) -> bytes:
-    def unescape() -> Iterator[bytes]:
-        previous_byte = None
-
-        for byte in header_buffer:
-            if previous_byte == b"\\":
-                yield REVERSE_ESCAPE_CHARS.get(byte, byte)
-            elif byte != b"\\":
-                yield byte
-
-            previous_byte = byte
-
-    return b"".join(unescape())
-
-
 def separate_complete_and_incomplete_packet_parts(raw_frames: bytes) -> tuple[bytes, bytes]:
     if raw_frames.endswith(EOF_MARKER) or raw_frames.replace(b"\n", b"") == b"":
         return (raw_frames, b"")
     parts = raw_frames.rpartition(EOF_MARKER)
     return parts[0] + parts[1], parts[2]
-
-
-def split_lines(buffer: deque[bytes]) -> Iterator[deque[bytes]]:
-    def parse_one_line() -> Iterator[bytes]:
-        previous_byte = None
-
-        while buffer:
-            byte = buffer.popleft()
-
-            if byte == NEWLINE:
-                if previous_byte and previous_byte != CARRIAGE:
-                    yield previous_byte
-                break
-
-            if previous_byte:
-                yield previous_byte
-            previous_byte = byte
-
-    while True:
-        current_line = deque[bytes]()
-        for byte in parse_one_line():
-            current_line.append(byte)
-        yield current_line
 
 
 def parse_command(raw_frame: deque[bytes]) -> str:
@@ -113,6 +75,21 @@ def parse_command(raw_frame: deque[bytes]) -> str:
             previous_byte = byte
 
     return b"".join(parse()).decode()
+
+
+def unescape_header(header_buffer: list[bytes]) -> bytes:
+    def unescape() -> Iterator[bytes]:
+        previous_byte = None
+
+        for byte in header_buffer:
+            if previous_byte == b"\\":
+                yield REVERSE_ESCAPE_CHARS.get(byte, byte)
+            elif byte != b"\\":
+                yield byte
+
+            previous_byte = byte
+
+    return b"".join(unescape())
 
 
 def parse_headers(raw_frame: deque[bytes]) -> dict[str, str]:
@@ -158,35 +135,34 @@ def parse_body(raw_frame: deque[bytes]) -> bytes:
 
 
 def load_frames(raw_frames: bytes) -> Iterator[AnyFrame]:
-    raw_frames_deque = deque(struct.unpack(f"{len(raw_frames)!s}c", raw_frames))
+    buffer = deque(struct.unpack(f"{len(raw_frames)!s}c", raw_frames))
 
-    while raw_frames_deque:
-        first_byte = raw_frames_deque.popleft()
-        if first_byte == b"\n":
+    while buffer:
+        if (first_byte := buffer.popleft()) == NEWLINE:
             yield HeartbeatFrame(headers={})
+            continue
+        buffer.appendleft(first_byte)
+
+        try:
+            command = parse_command(buffer)
+        except IndexError:
+            return
+        if not buffer:
+            return
+
+        try:
+            headers = parse_headers(buffer)
+        except IndexError:
+            return
+        if not buffer:
+            return
+
+        try:
+            body = parse_body(buffer)
+        except IndexError:
+            return
+
+        if known_frame_type := COMMANDS_TO_FRAME_TYPES.get(command):
+            yield known_frame_type(headers=cast(Any, headers), body=body)
         else:
-            raw_frames_deque.appendleft(first_byte)
-
-            try:
-                command = parse_command(raw_frames_deque)
-            except IndexError:
-                return
-            if not raw_frames_deque:
-                return
-
-            try:
-                headers = parse_headers(raw_frames_deque)
-            except IndexError:
-                return
-            if not raw_frames_deque:
-                return
-
-            try:
-                body = parse_body(raw_frames_deque)
-            except IndexError:
-                return
-
-            if known_frame_type := COMMANDS_TO_FRAME_TYPES.get(command):
-                yield known_frame_type(headers=cast(Any, headers), body=body)
-            else:
-                yield UnknownFrame(command=command, headers=headers, body=body)
+            yield UnknownFrame(command=command, headers=headers, body=body)
