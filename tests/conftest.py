8eb7b86861ee754d703815d1f2ca1bfffc2403b3
Lev Vereshchagin <mail@vrslev.com>
2024-06-15 01:19:36 +0300

    Test with uvloop

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,12 @@
 import pytest
 
 
-@pytest.fixture(scope="session", autouse=True)
-def anyio_backend() -> str:
-    return "asyncio"
+@pytest.fixture(
+    params=[
+        pytest.param(("asyncio", {"use_uvloop": True}), id="asyncio+uvloop"),
+        pytest.param(("asyncio", {"use_uvloop": False}), id="asyncio"),
+    ],
+    autouse=True,
+)
+def anyio_backend(request: pytest.FixtureRequest) -> object:
+    return request.param
