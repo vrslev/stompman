d8490821d2329468b74f7fcdc54150dae7ad4c6f
Lev Vereshchagin <mail@vrslev.com>
2024-08-12 23:30:26 +0300

    Update

--- tests/test_connection_manager_pure.py
+++ tests/test_connection_manager_pure.py
@@ -5,8 +5,20 @@ from unittest import mock
 import pytest
 
 from stompman import FailedAllConnectAttemptsError
-from stompman.connection_manager import ActiveConnectionState, attempt_to_connect, connect_to_first_server
-from stompman.errors import AnyConnectionIssue
+from stompman.config import ConnectionParameters
+from stompman.connection_manager import (
+    ActiveConnectionState,
+    attempt_to_connect,
+    connect_to_first_server,
+    make_healthy_connection,
+)
+from stompman.errors import (
+    AllServersUnavailable,
+    AnyConnectionIssue,
+    ConnectionLost,
+    ConnectionLostError,
+    StompProtocolConnectionIssue,
+)
 from tests.conftest import build_dataclass
 from tests.test_connection_manager import FAKER
 
@@ -73,4 +85,39 @@ async def test_connect_to_first_server_fails() -> None:
             return_argument_async(None),
         ]
     )
+
     assert active_connection_state is None
+
+
+async def test_make_healthy_connection_no_active_state() -> None:
+    servers = [build_dataclass(ConnectionParameters) for _ in range(FAKER.pyint(max_value=10))]
+    connect_timeout = FAKER.pyint()
+    result = await make_healthy_connection(
+        active_connection_state=None, servers=servers, connect_timeout=connect_timeout
+    )
+
+    assert result == AllServersUnavailable(servers=servers, timeout=connect_timeout)
+
+
+async def test_make_healthy_connection_connection_lost() -> None:
+    active_connection_state = ActiveConnectionState(
+        connection=mock.AsyncMock(), lifespan=mock.AsyncMock(enter=mock.AsyncMock(side_effect=ConnectionLostError))
+    )
+    result = await make_healthy_connection(
+        active_connection_state=active_connection_state, servers=[], connect_timeout=0
+    )
+
+    assert result == ConnectionLost()
+
+
+@pytest.mark.parametrize("issue_type", get_args(StompProtocolConnectionIssue))
+async def test_make_healthy_connection_stomp_protocol_issue(issue_type: type[StompProtocolConnectionIssue]) -> None:
+    issue = build_dataclass(issue_type)
+    active_connection_state = ActiveConnectionState(
+        connection=mock.AsyncMock(), lifespan=mock.AsyncMock(enter=mock.AsyncMock(side_effect=[issue]))
+    )
+    result = await make_healthy_connection(
+        active_connection_state=active_connection_state, servers=[], connect_timeout=0
+    )
+
+    assert result == issue
