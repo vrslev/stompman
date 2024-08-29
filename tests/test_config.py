d05497ec81fdb164f06fa1be7d5270b40e37f9d7
Lev Vereshchagin <mail@vrslev.com>
2024-08-29 14:22:19 +0300

    Update

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,56 +1,46 @@
-from typing import TypedDict, cast
+from typing import TYPE_CHECKING, Literal
 
 import faker
 import pytest
-from polyfactory.factories.typed_dict_factory import TypedDictFactory
 
 import stompman
-from stompman.config import MultiHostHostLike
+
+if TYPE_CHECKING:
+    from stompman.config import MultiHostHostLike
 
 
-class StrictMultiHostHostLike(TypedDict):
-    username: str
-    password: str
-    host: str
-    port: int
+class TestConnectionParametersFromPydanticMultiHostHosts:
+    def test_ok(self, faker: faker.Faker) -> None:
+        hosts: list[MultiHostHostLike] = [
+            {"host": "host1", "port": 1, "username": None, "password": None},
+            {"host": "host2", "port": 2, "username": None, "password": None},
+            {"host": "host3", "port": 3, "username": None, "password": None},
+            {"host": "host4", "port": 4, "username": None, "password": None},
+        ]
+        host_with_credentials = faker.pyint(min_value=0, max_value=3)
+        hosts[host_with_credentials]["username"] = "lev"
+        hosts[host_with_credentials]["password"] = "pass"  # noqa: S105
 
+        result = stompman.ConnectionParameters.from_pydantic_multihost_hosts(hosts)
 
-class StrictMultiHostHostLikeFactory(TypedDictFactory[StrictMultiHostHostLike]): ...
+        assert result == [
+            stompman.ConnectionParameters("host1", 1, "lev", "pass"),
+            stompman.ConnectionParameters("host2", 2, "lev", "pass"),
+            stompman.ConnectionParameters("host3", 3, "lev", "pass"),
+            stompman.ConnectionParameters("host4", 4, "lev", "pass"),
+        ]
 
+    @pytest.mark.parametrize("key", ["host", "port"])
+    def test_fails_one_host_parsing(self, faker: faker.Faker, key: Literal["host", "port"]) -> None:
+        hosts: list[MultiHostHostLike] = [
+            {
+                "host": faker.pystr(),
+                "port": faker.pyint(),
+                "username": faker.pystr(),
+                "password": faker.pystr(),
+                key: None,  # type: ignore[misc]
+            }
+        ]
 
-def test_connection_parameters_from_pydantic_multihost_hosts_ok(faker: faker.Faker) -> None:
-    hosts: list[MultiHostHostLike] = [
-        {"host": "host1", "port": 1, "username": None, "password": None},
-        {"host": "host2", "port": 2, "username": None, "password": None},
-        {"host": "host3", "port": 3, "username": None, "password": None},
-        {"host": "host4", "port": 4, "username": None, "password": None},
-    ]
-    host_with_credentials = faker.pyint(min_value=0, max_value=3)
-    hosts[host_with_credentials]["username"] = "lev"
-    hosts[host_with_credentials]["password"] = "pass"  # noqa: S105
-
-    result = stompman.ConnectionParameters.from_pydantic_multihost_hosts(hosts)
-
-    assert result == [
-        stompman.ConnectionParameters("host1", 1, "lev", "pass"),
-        stompman.ConnectionParameters("host2", 2, "lev", "pass"),
-        stompman.ConnectionParameters("host3", 3, "lev", "pass"),
-        stompman.ConnectionParameters("host4", 4, "lev", "pass"),
-    ]
-
-
-@pytest.mark.parametrize("key", ["host", "port"])
-def test_connection_parameters_from_pydantic_multihost_hosts_fails_one_host_parsing() -> None:
-    server = StrictMultiHostHostLikeFactory.build()
-
-    for key in ("host", "port", "username", "password"):
         with pytest.raises(ValueError, match=f"{key} must be set"):
-            assert stompman.ConnectionParameters.from_pydantic_multihost_hosts(
-                [
-                    {
-                        **server,
-                        key: None,  # type: ignore[misc]
-                    },
-                    cast(MultiHostHostLike, server),
-                ],
-            )
+            stompman.ConnectionParameters.from_pydantic_multihost_hosts(hosts)
